2fb84a826737e29e9a5cb33bd7fe39547f4a303b
aureoleday <ztscotty@gmail.com>
2020-05-28 11:32:21 +0800

    add iwdg timer, set to 2.2s cooldown, and refresh period 1s

--- bsp/stm32/stm32f103_infared_rx/applications/main.c
+++ bsp/stm32/stm32f103_infared_rx/applications/main.c
@@ -19,6 +19,31 @@
 
 /* defined the LED0 pin: PB1 */
 #define LED0_PIN    GET_PIN(B, 15)
+IWDG_HandleTypeDef hiwdg;
+
+static void MX_IWDG_Init(void)
+{
+
+  /* USER CODE BEGIN IWDG_Init 0 */
+
+  /* USER CODE END IWDG_Init 0 */
+
+  /* USER CODE BEGIN IWDG_Init 1 */
+
+  /* USER CODE END IWDG_Init 1 */
+  hiwdg.Instance = IWDG;
+  hiwdg.Init.Prescaler = IWDG_PRESCALER_64;
+  //hiwdg.Init.Window = 4095;
+  hiwdg.Init.Reload = 2200;
+  if (HAL_IWDG_Init(&hiwdg) != HAL_OK)
+  {
+    Error_Handler();
+  }
+  /* USER CODE BEGIN IWDG_Init 2 */
+
+  /* USER CODE END IWDG_Init 2 */
+
+}
 
 int main(void)
 {
@@ -30,12 +55,14 @@ int main(void)
     dac_start();
     amux_drv_init();
     sr_init();
+    MX_IWDG_Init();
     while (count++)
     {
         rt_pin_write(LED0_PIN, PIN_HIGH);
         rt_thread_mdelay(990);
         rt_pin_write(LED0_PIN, PIN_LOW);
         rt_thread_mdelay(10);
+        HAL_IWDG_Refresh(&hiwdg);
     }
 
     return RT_EOK;
